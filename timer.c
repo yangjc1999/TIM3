f84f2bdd709da94c354dbc94fb5ce63d094a2e84
yangjc1999 <52656404+yangjc1999@users.noreply.github.com>
2019-08-22 17:02:30 +0800

    c

---
+++ timer.c
@@ -0,0 +1,50 @@
+#include "timer.h"
+#include "led.h"
+
+void TIM3_Int_Init(u16 arr,u16 psc)
+{
+    TIM_TimeBaseInitTypeDef  TIM_TimeBaseStructure;
+	NVIC_InitTypeDef NVIC_InitStructure;
+
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE); //ʱ��ʹ��
+	
+	//��ʱ��TIM3��ʼ��
+	TIM_TimeBaseStructure.TIM_Period = arr; //��������һ�������¼�װ������Զ���װ�ؼĴ������ڵ�ֵ	
+	TIM_TimeBaseStructure.TIM_Prescaler =psc; //����������ΪTIMxʱ��Ƶ�ʳ�����Ԥ��Ƶֵ
+	TIM_TimeBaseStructure.TIM_ClockDivision = TIM_CKD_DIV1; //����ʱ�ӷָ�:TDTS = Tck_tim
+	TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;  //TIM���ϼ���ģʽ
+	TIM_TimeBaseInit(TIM3, &TIM_TimeBaseStructure); //����ָ���Ĳ�����ʼ��TIMx��ʱ��������λ
+ 
+	TIM_ITConfig(TIM3,TIM_IT_Update,ENABLE ); //ʹ��ָ����TIM3�ж�,���������ж�
+
+	//�ж����ȼ�NVIC����
+	NVIC_InitStructure.NVIC_IRQChannel = TIM3_IRQn;  //TIM3�ж�
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;  //��ռ���ȼ�0��
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 3;  //�����ȼ�3��
+	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE; //IRQͨ����ʹ��
+	NVIC_Init(&NVIC_InitStructure);  //��ʼ��NVIC�Ĵ���
+
+
+	TIM_Cmd(TIM3, ENABLE);  //ʹ��TIMx					 
+}
+//��ʱ��3�жϷ�������
+void TIM3_IRQHandler(void)   //TIM3�ж�
+{
+	if (TIM_GetITStatus(TIM3, TIM_IT_Update) != RESET)  //����TIM3�����жϷ�������
+		{
+		TIM_ClearITPendingBit(TIM3, TIM_IT_Update  );  //����TIMx�����жϱ�־ 
+		LED1=!LED1;
+		}
+}
+
+
+
+
+
+
+
+
+
+
+
+
